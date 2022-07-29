c8c3bb1040e8a89d45a63965d49864f5b8a2cfd3
crisxchan <chrisgonzales.online@gmail.com>
2022-07-29 20:28:57 +0800

    Add conditional when setup is not provided on joke

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ function App() {
 
   return (
     <>
+      <Joke 
+        punchline="It’s hard to explain puns to kleptomaniacs because they always take things literally." 
+      />
       <Joke
         setup = 'I got my daughter a fridge for her birthday.'
         punchline= "I can't wait to see her face light up when she opens it."
--- src/component/Joke.jsx
+++ src/component/Joke.jsx
@@ -3,7 +3,7 @@ import './joke.css'
 export default function Joke(props) {
     return (
         <div id="joke">
-            <h1 id="setup">{props.setup}</h1>
+            {props.setup && <h1 id="setup">{props.setup}</h1>}
             <h1 id="punchline">{props.punchline}</h1>
         </div>
     )
